c31779c08ed575259299994bcb073488e11a25a3
WilckerNeckel <wilckerwrsn@gmail.com>
2025-09-15 23:06:17 -0400

    feat: implements password vo

---
+++ src/modules/shared/domain/vos/Password.ts
@@ -0,0 +1,43 @@
+import bcrypt from "bcrypt";
+import z from "zod";
+
+export class GestorPassword {
+    private readonly hashedPassword: string;
+
+    private constructor(hashedPassword: string) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    // Define password validation validator inside the GestorPassword class
+    private static validate(plainTextPassword: string) {
+        const validator = z
+            .string()
+            .min(6, "A senha deve conter pelo menos 6 caracteres");
+
+        validator.parse(plainTextPassword); // Throws an error if invalid
+    }
+
+    //Method to create a hashed password from plain text
+    static async create(plainTextPassword: string): Promise<GestorPassword> {
+        GestorPassword.validate(plainTextPassword);
+        const salt = 10;
+        const hashedPassword = await bcrypt.hash(plainTextPassword, salt);
+        return new GestorPassword(hashedPassword);
+    }
+
+    /**
+     * Cria Senha sem hashear novamente, pois já está hasheado
+     *
+     */
+    static fromHash(hashedPassword: string): GestorPassword {
+        return new GestorPassword(hashedPassword);
+    }
+
+    async matches(plainTextPassword: string): Promise<boolean> {
+        return await bcrypt.compare(plainTextPassword, this.hashedPassword);
+    }
+
+    get value(): string {
+        return this.hashedPassword;
+    }
+}
