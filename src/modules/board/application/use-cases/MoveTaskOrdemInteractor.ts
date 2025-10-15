9c90a7c9acbec9703b09e671074a8670c171aa17
WilckerNeckel <wilckerwrsn@gmail.com>
2025-10-14 22:44:21 -0400

    refactor: format code

--- src/modules/board/application/use-cases/MoveTaskOrdemInteractor.ts
+++ src/modules/board/application/use-cases/MoveTaskOrdemInteractor.ts
@@ -1,17 +1,13 @@
 import { BoardGateway } from "../../domain/ports/BoardGateway";
 
-export class MoveTaskOrdemInteractor{
-    constructor(private boardGateway: BoardGateway){}
-    async execute(
-        taskId: string,
-        columnId: string,
-        newPosition: number,
-    ): Promise<void> {
+export class MoveTaskOrdemInteractor {
+    constructor(private boardGateway: BoardGateway) {}
+    async execute(taskId: string, newPosition: number): Promise<void> {
         const currentTask = await this.boardGateway.getTaskById(taskId);
         if (!currentTask) throw new Error("Tarefa não encontrada");
 
         const currentPosition = currentTask.ordem;
-
+        const columnId = currentTask.columnId;
 
         await this.boardGateway.moveTaskOrdem(
             taskId,
